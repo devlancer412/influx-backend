12c968888bf692283c5a3396425a0415a86e4d13
mbenz <mbenz10120112@gmail.com>
2022-09-19 09:45:15 -0400

    add totalFollowers in each campaign

--- controllers/campaigns.controller.js
+++ controllers/campaigns.controller.js
@@ -5,20 +5,20 @@ const prisma = new PrismaClient()
 const store = async (req, res) => {
   // To Do:
   try {
-    const { name, avgER, creator } = req.body;
+    const { name, avgER, creator } = req.body
     const newCampaign = await prisma.campaign.create({
       data: {
         name,
         avgER,
-        creator
-      }
-    });
-  
-    res.json(newCampaign);  
+        creator,
+      },
+    })
+
+    res.json(newCampaign)
   } catch (error) {
     console.log(error)
     res.json(error)
-  }  
+  }
 }
 
 const getList = async (req, res) => {
@@ -28,17 +28,17 @@ const getList = async (req, res) => {
 
     const account = await prisma.account.findUnique({
       where: {
-        id: accountId
+        id: accountId,
       },
       include: {
         influencer: {
           include: {
             campaigns: {
               include: {
-                campaign: true
-              }
-            }
-          }
+                campaign: true,
+              },
+            },
+          },
         },
         brand: {
           include: {
@@ -48,16 +48,16 @@ const getList = async (req, res) => {
                   include: {
                     influencer: {
                       include: {
-                        account: true
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
+                        account: true,
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
     })
 
     let campaigns = []
@@ -68,11 +68,11 @@ const getList = async (req, res) => {
       campaigns = account.influencer.campaigns
     }
 
-    res.json(campaigns); 
+    res.json(campaigns)
   } catch (error) {
     console.log(error)
     res.json(error)
-  }  
+  }
 }
 
 const getById = async (req, res) => {
@@ -82,21 +82,38 @@ const getById = async (req, res) => {
 
     const campaign = await prisma.campaign.findUnique({
       where: {
-        id
+        id,
       },
       include: {
         influencers: {
           include: {
             influencer: {
               include: {
-                account: true
-              }
-            }
-          }
-        }
-      }
+                account: {
+                  include: {
+                    telegram: true,
+                    youtube: true,
+                    tiktok: true,
+                    twitter: true,
+                    instagram: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
     })
-    
+
+    let totalFollowers = 0;
+    campaign.influencers.forEach(influencer => {
+      if (influencer.influencer.account.telegram) totalFollowers += influencer.influencer.account.telegram.channelMembers;
+      if (influencer.influencer.account.instagram) totalFollowers += influencer.influencer.account.instagram.followers;
+      if (influencer.influencer.account.youtube) totalFollowers += influencer.influencer.account.youtube.subscribers;
+      if (influencer.influencer.account.twitter) totalFollowers += influencer.influencer.account.twitter.followers;
+      if (influencer.influencer.account.tiktok) totalFollowers += influencer.influencer.account.tiktok.followers;
+    })
+    campaign.totalFollowers = totalFollowers;
     res.json(campaign)
   } catch (error) {
     console.log(error)
@@ -106,40 +123,40 @@ const getById = async (req, res) => {
 
 const addInfluencer = async (req, res) => {
   try {
-      const { campaignId, influencerId, status, negotiatedBudget } = req.body
-      const campaign = await prisma.campaign.findUnique({
-        where:{
-          id: campaignId
-        }
-      })
+    const { campaignId, influencerId, status, negotiatedBudget } = req.body
+    const campaign = await prisma.campaign.findUnique({
+      where: {
+        id: campaignId,
+      },
+    })
 
-      const influencer = await prisma.influencer.findUnique({
-        where: {
-          id: influencerId
-        }
-      })
+    const influencer = await prisma.influencer.findUnique({
+      where: {
+        id: influencerId,
+      },
+    })
 
-      if (!campaign || !influencer) return res.json("error")
+    if (!campaign || !influencer) return res.json('error')
 
-      await prisma.campaignInfluencer.create({
-        data:{
-          campaignId,
-          influencerId,
-          status,
-          negotiatedBudget
-        }
-      });
+    await prisma.campaignInfluencer.create({
+      data: {
+        campaignId,
+        influencerId,
+        status,
+        negotiatedBudget,
+      },
+    })
 
-      await prisma.campaign.update({
-        where: {
-          id: campaignId
-        },
-        data: {
-          negoBudget: campaign.negoBudget + negotiatedBudget
-        }
-      })
+    await prisma.campaign.update({
+      where: {
+        id: campaignId,
+      },
+      data: {
+        negoBudget: campaign.negoBudget + negotiatedBudget,
+      },
+    })
 
-      res.json("success")
+    res.json('success')
   } catch (error) {
     console.log(error)
     res.json(error)
@@ -150,5 +167,5 @@ module.exports = {
   store,
   getList,
   addInfluencer,
-  getById
+  getById,
 }
