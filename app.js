db5b045e8f4fd246d1fc3b8fb62d863df686be73
mbenz <mbenz10120112@gmail.com>
2022-10-04 12:26:38 -0400

    deploy on ec2

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const app = require('./config/express');
 const routes = require('./routes/index.route');
-const socialCtr = require('./controllers/social.controller');
-const serverless = require('serverless-http');
+// const socialCtr = require('./controllers/social.controller');
+// const serverless = require('serverless-http');
 
 // Router
 app.get('/test', (req, res) => res.json('Test'));
 app.use('/api', routes);
 
-exports.handler = serverless(app);
+// exports.handler = serverless(app);
 // socialCtr.storeTelegram();
 // socialCtr.storeTiktok();
 // socialCtr.storeTwitter();
 // socialCtr.storeInstagram('socialblade', 1);
 // socialCtr.storeYoutube();
 
-// if (process.env.NODE_ENV === 'development') {
-//   app.listen(app.get('port'), () => {
-//     console.log(
-//       `Server running at http://${app.get('host')}:${app.get('port')}`
-//     );
-//   });
-// } else {
-//   //
-// }
+if (process.env.NODE_ENV === 'development') {
+  app.listen(app.get('port'), () => {
+    console.log(
+      `Server running at http://${app.get('host')}:${app.get('port')}`
+    );
+  });
+} else {
+  //
+}
